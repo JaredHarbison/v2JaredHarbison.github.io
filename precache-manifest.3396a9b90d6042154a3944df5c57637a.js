990021b02a3d00091674e86ab3fa97bcaa84a2a0
Jared Harbison <jared.harbison@gmail.com>
2023-11-29 22:49:01 -0500

    Updates

---
+++ precache-manifest.3396a9b90d6042154a3944df5c57637a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43fa15c57db9a60952973fcb91bb5a9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dec97b0159a097accf74",
+    "url": "/static/css/main.eb98c8f8.chunk.css"
+  },
+  {
+    "revision": "74e3bc71ffd39ee22a98",
+    "url": "/static/js/2.f700388b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f700388b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec97b0159a097accf74",
+    "url": "/static/js/main.9f92e636.chunk.js"
+  },
+  {
+    "revision": "e38377c73994b0c3f24c",
+    "url": "/static/js/runtime-main.3804b618.js"
+  },
+  {
+    "revision": "5a41dcc3f442b0de24042163c38d13c5",
+    "url": "/static/media/rom-and-moon.5a41dcc3.png"
+  },
+  {
+    "revision": "9a14d6b98a9733dd77e131fc39b5bbbc",
+    "url": "/static/media/rom-cutout-downside.9a14d6b9.png"
+  },
+  {
+    "revision": "80c29627081ad27fd6ca0e0be148668d",
+    "url": "/static/media/rom-cutout-upside.80c29627.png"
+  }
+]);
